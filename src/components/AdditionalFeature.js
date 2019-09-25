3d731e0fbff97ee232b869d88dd386b324e5d6c8
MicahJank <MicahJank@users.noreply.github.com>
2019-09-24 22:32:14 -0500

    made it so the add buttons cant be pressed a second time

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -3,7 +3,13 @@ import React from 'react';
 const AdditionalFeature = props => {
 
   const buttonHandler = () => {
-    props.buyFeature(props.feature);
+    // The user should only be able to enter in a feature once, this if statement will check if the features added key is false and
+    // if it is the user can use the buy feature function...if not the user cant...basically this disables the button so the user
+    // cant keep buying the item over and over.
+    if(!props.feature.added) {
+      props.buyFeature(props.feature);
+    }
+    
   }
 
   return (
--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,15 +1,26 @@
-// action variables imported here
+import {ADD_FEATURE, REMOVE_FEATURE} from '../actions';
 
 const initialState = [
-    { id: 1, name: 'V-6 engine', price: 1500 },
-    { id: 2, name: 'Racing detail package', price: 1500 },
-    { id: 3, name: 'Premium sound system', price: 500 },
-    { id: 4, name: 'Rear spoiler', price: 250 }
+    { id: 1, name: 'V-6 engine', price: 1500, added: false },
+    { id: 2, name: 'Racing detail package', price: 1500, added: false },
+    { id: 3, name: 'Premium sound system', price: 500, added: false },
+    { id: 4, name: 'Rear spoiler', price: 250, added: false }
   ];
 
    const store = (state = initialState, action) => {
       switch(action.type) {
-        // cases
+        case ADD_FEATURE:
+          return state.map(item => {
+            if(item.id === action.payload.id) {
+              return {...item, added: true};
+            } else return item;
+          })
+        case REMOVE_FEATURE:
+            return state.map(item => {
+              if(item.id === action.payload.id) {
+                return {...item, added: false};
+              } else return item;
+            })
         default:
             return state;
       }
